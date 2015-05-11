91dc8bbd7e6f35315477b59f41a6aaec03246ae5
naughtyFish <1412168126@qq.com>
2015-05-11 13:57:56 +0800

    before lp

---
+++ EveryoneNews/Category/NSDate+LP.m
@@ -0,0 +1,46 @@
+
+//
+//  NSDate+LP.m
+//  testString
+//
+//  Created by apple on 15/5/8.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import "NSDate+LP.h"
+
+@implementation NSDate (LP)
+
++ (NSDate *)localeDate
+{
+    NSDate *date = [NSDate date];
+    NSTimeZone *zone = [NSTimeZone systemTimeZone];
+    NSInteger interval = [zone secondsFromGMTForDate:date];
+    return [date dateByAddingTimeInterval:interval];
+}
+
++ (NSString *)dateStringSince:(int)i join:(NSString *)join
+{
+    NSString *month = @"";
+    NSString *day = @"";
+    
+    
+    NSDate *date = [NSDate dateWithTimeIntervalSinceNow:-(i*24*3600)];
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:date];
+    if (components.month < 10) {
+        month = [NSString stringWithFormat:@"0%ld", components.month];
+    } else {
+        month = [NSString stringWithFormat:@"%ld", components.month];
+    }
+    if (components.day < 10) {
+        day = [NSString stringWithFormat:@"0%ld", components.day];
+    } else {
+        day = [NSString stringWithFormat:@"%ld", components.day];
+    }
+    return [NSString stringWithFormat:@"%@%@%@", month, join, day];
+
+}
+
+@end
---
+++ EveryoneNews/CountdownView/CountdownView.h
@@ -0,0 +1,17 @@
+//
+//  CountdownView.h
+//  EveryoneNews
+//
+//  Created by apple on 15/5/6.
+//  Copyright (c) 2015年 yyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountdownView : UIView
+
+@property (nonatomic, assign) int updateTime;
+
+@property (nonatomic, assign) BOOL type;
+
+@end
