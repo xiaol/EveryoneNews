4c2148a2819d7266a429ce1b679296706cc6089c
naughtyFish <1412168126@qq.com>
2015-05-19 10:55:42 +0800

    delete blue effect

--- Pods/Headers/Public/GRKBlurView/GRKBlurView.h
+++
@@ -1,1 +0,0 @@
-../../../GRKBlurView/GRKBlurView/GRKBlurView.h
--- Pods/Target Support Files/Pods-GRKBlurView/Pods-GRKBlurView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GRKBlurView : NSObject
-@end
-@implementation PodsDummy_Pods_GRKBlurView
-@end
