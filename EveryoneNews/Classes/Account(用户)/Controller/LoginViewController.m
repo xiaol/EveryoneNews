aab39c0b1c19afe5a7800823c49ae590ac1aa076
feng <fly_elephant@163.com>
2015-06-30 17:13:39 +0800

    added:user login

--- EveryoneNews/Classes/Account(用户)/Controller/LoginViewController.m
+++ EveryoneNews/Classes/Account(用户)/Controller/LoginViewController.m
@@ -10,7 +10,10 @@
 #import "AccountTool.h"
 #import "MBProgressHUD+MJ.h"
 #import "UIImage+LP.h"
+
+
 @interface LoginViewController ()
+@property (nonatomic,strong) UIView *wrapperView;
 
 @end
 
@@ -33,24 +36,42 @@
     mask.backgroundColor=[UIColor colorFromHexString:@"0000000" alpha:0.85];
     [self.view addSubview:mask];
     [self setupSubviews];
+
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [UIView animateWithDuration:0.8 animations:^{
+        self.wrapperView.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        
+    }];
 }
 /**
  *  初始化和添加子views
  */
 - (void)setupSubviews{
+    UIView *viewWrapper = [[UIView alloc] init];
+    [self.view addSubview:viewWrapper];
+    self.wrapperView = viewWrapper;
+    viewWrapper.frame = self.view.bounds;
+    viewWrapper.backgroundColor = [UIColor clearColor];
+    
     UILabel *firstLabel = [[UILabel alloc] init];
     firstLabel.text = @"加入百家";
     firstLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:20.0];
     firstLabel.frame=CGRectMake((ScreenWidth-230.0 / 375 * ScreenWidth) * 0.5 , ScreenHeight * 0.3, 100, 20);
     firstLabel.textColor=[UIColor whiteColor];
-    [self.view addSubview:firstLabel];
+    [viewWrapper addSubview:firstLabel];
     
     UILabel *secondLabel = [[UILabel alloc] init];
     secondLabel.text = @"与世界分享你的真知灼见";
     secondLabel.font = [UIFont systemFontOfSize:15.0];
     secondLabel.frame=CGRectMake((ScreenWidth-230.0 / 375 * ScreenWidth) * 0.5 , CGRectGetMaxY(firstLabel.frame) + 14, 200, 15);
     secondLabel.textColor=[UIColor whiteColor];
-    [self.view addSubview:secondLabel];
+    [viewWrapper addSubview:secondLabel];
     
     //微博登录按钮
     UIButton *weiboBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -65,7 +86,7 @@
      [weiboBtn setImage:[UIImage imageNamed:@"ic_login_weibo"] forState:UIControlStateHighlighted];
     [weiboBtn setTitle:@"使用微博账号" forState:UIControlStateNormal];
     [weiboBtn addTarget:self action:@selector(weiboLogin:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:weiboBtn];
+    [viewWrapper addSubview:weiboBtn];
     
     //微信登录按钮
     UIButton *weixinBtn = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -80,14 +101,17 @@
     [weixinBtn setImage:[UIImage imageNamed:@"ic_login_weixin"] forState:UIControlStateHighlighted];
     [weixinBtn setTitle:@"使用微信账号" forState:UIControlStateNormal];
     [weixinBtn addTarget:self action:@selector(weixinLogin:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:weixinBtn];
+    [viewWrapper addSubview:weixinBtn];
     
     //添加关闭按钮
     UIButton *closeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     closeBtn.frame = CGRectMake(ScreenWidth * 0.5 - 13, CGRectGetMaxY(weixinBtn.frame)+ 24, 25, 25);
     [closeBtn setBackgroundImage:[UIImage imageNamed:@"ic_login_close"] forState:UIControlStateNormal];
     [closeBtn addTarget:self action:@selector(closeSelf) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:closeBtn];
+    [viewWrapper addSubview:closeBtn];
+    viewWrapper.alpha = 0.5;
+
+    
     
 }
 /**
@@ -120,10 +144,6 @@
     }];
   
 }
-- (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion{
-    NSLog(@"presentViewController");
-    [super presentViewController:viewControllerToPresent animated:flag completion:completion];
-    
-}
+
 @end
 
